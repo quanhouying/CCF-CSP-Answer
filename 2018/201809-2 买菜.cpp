cd1e6ac226a1ce03552c57b412c7948de6f509cf
quanhouying <88315736+quanhouying@users.noreply.github.com>
2023-04-05 15:22:57 +0800

    Update 201809-2 买菜.cpp

--- 2018/201809-2 买菜.cpp
+++ 2018/201809-2 买菜.cpp
@@ -4,8 +4,23 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);   //提高cin,cout的速度
-    int n;
-    
+    int n, t, time[2000][2] = {0}, count = 0;
+	cin >> n;
+	for (int i = 0;i < n;i++) {
+		cin >> time[i][0] >> time[i][1];
+	}
+	for (int i = 0;i < n;i++) {
+		int l, r;
+		cin >> l >> r;
+		for (t = 0;t < 2000;t++) {
+			if (time[t][0] > r)
+				break;
+			if (time[t][1] < l)
+				continue;
+			count += min(time[t][1], r) - max(time[t][0], l);
+		}
+	}
+    cout << count;
 
     return 0;
 }
